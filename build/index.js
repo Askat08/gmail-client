5b826017a6295f80fc28479c71ef2cb466f98b44
Aidana Khankel <aidanakhankeldieva91@gmail.com>
2020-09-28 18:20:49 -0500

    init com

--- build/index.js
+++ build/index.js
@@ -1,1 +1,17 @@
-const test = 'This is a text';
+
+import axios from 'axios';
+
+console.log('This is a text');
+
+//Aidana's code
+
+async function getData() {
+    const d = await axios.get("https://polar-reaches-49806.herokuapp.com/api?page=1&category=primary").then(response => {
+        console.log(response);
+    }).catch(err => {
+        console.log('error', err);
+    }); //
+    console.log("Hello");
+}
+
+getData();
